236ca0ba2f22fcc6307c367478bfb50b463eb3c2
marcia <marcia.millanao@gmail.com>
2016-11-07 09:19:38 -0300

    arreglo estilo de iconos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function nuevaTarea(){
 	var contenedor = document.getElementById('contenedor_nuevatarea');
-	document.getElementById('contenedor_nuevatarea').value=" ";
 	var textarea = document.getElementById('texto').value;
+	document.getElementById('texto').value="";
 	//COMPARA QUE TAREA SEA VALIDA
 	if(textarea == null || textarea.length == 0){
 		alert("ingrese tarea");
@@ -15,8 +15,9 @@ function nuevaTarea(){
 	var eliminar_span = document.createElement('i');//icono basurero
 	var corazon_span = document.createElement('i');//icono corazon
 
-	container_div.classList.add("div", "col-md-12");
+	container_div.classList.add("lista", "row");
 	checkbox_span.setAttribute("type", "checkbox");
+	contenedor_lista.classList.add("span", "text_lista")
 	eliminar_span.classList.add("glyphicon", "glyphicon-trash");
 	corazon_span.classList.add("glyphicon", "glyphicon-heart")
 	/*checkbox_span.type='checkbox';
@@ -24,22 +25,21 @@ function nuevaTarea(){
 	eliminar_span.setAttribute('class', 'glyphicon glyphicon-heart');
 	corazon_span.setAttribute('class', 'glyphicon glyphicon-heart');*/
 	//asigno hijos
-	
 	container_div.appendChild(checkbox_span);
 	contenedor_lista.appendChild(tex_span)
 	container_div.appendChild(contenedor_lista);
 	container_div.appendChild(eliminar_span);
 	container_div.appendChild(corazon_span);
 	contenedor.appendChild(container_div);
-
+	//tacho texto al seleccionar checkbox
 	checkbox_span.addEventListener("click", function(){
 		contenedor_lista.classList.toggle("tachado");
 	})
-
+	//pongo corazon de color al hacer click en el
 	corazon_span.addEventListener("click", function(){
 		corazon_span.classList.toggle("corazon");
 	})
-
+	//elimino basurero
 	eliminar_span.addEventListener("click", function(){
 		contenedor.removeChild(container_div);
 	})
